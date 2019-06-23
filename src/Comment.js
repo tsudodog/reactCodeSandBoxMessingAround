078f33d303b75963a3bc65dffeb8ca7c9246fde0
Alex <malamute29@gmail.com>
2019-06-23 15:20:09 -0500

    worked on basic comment container

--- src/Comment.js
+++ src/Comment.js
@@ -1,79 +1,52 @@
-import React from "react";
-import { render } from "react-dom";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import { Divider } from "@material-ui/core";
-import TextField from "@material-ui/core/TextField";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react'
+import { render } from 'react-dom'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import { Divider } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField'
+import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
 
 const styles = theme => {
   return {
     largeFont: {
-      fontSize: "30px"
+      fontSize: '30px'
     }
-  };
-};
+  }
+}
 // this is a fork
 const Comment = props => (
   <React.Fragment>
     <Paper style={{ marginBottom: 15 }} elevation="5">
-      <div
-        style={{
-          display: "grid",
-          marginTop: 10,
-          marginBottom: 5,
-          gridTemplateColumns: "1fr 3fr 1fr",
-          gridTemplateRows: "1fr 1fr"
-        }}
-      >
-        <div
-          style={{
-            gridColumnStart: 0,
-            gridColumnEnd: 1,
-            gridRowStart: 0,
-            gridRowEnd: 2
-          }}
-        >
+      <Grid style={{ paddingLeft: 10 }} container spacing={4}>
+        <Grid item style={{ paddingLeft: 10 }} xs={2}>
           <img
             alt=""
             style={{
               maxWidth: 100,
               maxHeight: 100,
-              display: "block",
-              marginLeft: "auto",
-              marginRight: "auto"
+              minWidth: 100,
+              minHeight: 100,
+              display: 'block',
+              marginLeft: 'auto',
+              marginRight: 'auto'
             }}
             src={props.imageSource}
           />
-        </div>
-        <div
-          style={{
-            gridColumnStart: 1,
-            gridColumnEnd: 2,
-            gridRowStart: 0,
-            gridRowEnd: 1
-          }}
-        >
-          <Typography style={{ textAlign: "left" }}>
+        </Grid>
+        <Grid item wrap="wrap" xs={10}>
+          <Typography style={{ textAlign: 'left' }}>
             {props.userName}
           </Typography>
-        </div>
-        <div
-          style={{
-            gridColumnStart: 1,
-            gridColumnEnd: 2,
-            gridRowStart: 1,
-            gridRowEnd: 2
-          }}
-        >
-          {/* <div className={props.classes.largeFont}>testTxt</div> */}
-          <Typography style={{ textAlign: "left" }}>
+          <Typography style={{ textAlign: 'left' }}>
             {props.commentText}
           </Typography>
-          <TextField />
-        </div>
-      </div>
+        </Grid>
+        <Grid item />
+      </Grid>
+
+      {/* <div className={props.classes.largeFont}>testTxt</div> */}
     </Paper>
   </React.Fragment>
-);
-export default withStyles(styles)(Comment);
+)
+export default withStyles(styles)(Comment)
--- src/CommentContainer.js
+++ src/CommentContainer.js
@@ -3,11 +3,14 @@ import Comment from './Comment'
 const exampleProp = [
   {
     imageSource: '//a.facdn.net/20190616/dragonighthawk.gif',
-    commentText: 'cringy rp goes here'
+    commentText: 'cringy rp goes here',
+    userName: 'user1'
   },
   {
     imageSource: '//a.facdn.net/1518896239/zanethefox.gif',
-    commentText: 'hello World'
+    commentText:
+      'hello World lorem ipusm longer text\nhello World lorem ipusm longer texthello World lorem ipusm longer texthello World lorem ipusm longer texthello World lorem ipusm longer texthello World lorem ipusm longer texthello World lorem ipusm longer text',
+    userName: 'SomeUser'
   }
 ]
 const CommentContainer = props => (
